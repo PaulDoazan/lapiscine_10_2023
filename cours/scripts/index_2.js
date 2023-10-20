cd12ac2b4053b746a64ac212447f6db08b499b0b
pdoazan <paul.doazan@gmail.com>
2023-10-20 12:15:38 +0200

    Add instructions for administrators exercice

--- cours/scripts/index_2.js
+++ cours/scripts/index_2.js
@@ -1,4 +1,4 @@
-let arrExample = [3, 5, 8, 12, 2, 43, 21, 2, 3, 42, 2, 5, 12, 555, 2, 43, 21, 2, 42, 2, 5, 12, 434]
+let arrExample = [3, 5, 8]
 
 let readFirstElement = true;
 
@@ -32,4 +32,75 @@ if (arrExample.length % 2 != 0) {
 
 let elementInTheMiddle = arrExample[middleIndex]
 
-console.log(elementInTheMiddle)
+// console.log(arrExample)
+
+// méthode pour ajouter un élément à la fin du tableau
+arrExample.push(51, 21, 24)
+
+// console.log(arrExample)
+
+// méthode pour retirer le dernier élément du tableau
+arrExample.pop()
+
+// console.log(arrExample)
+
+/**
+ * ----------------------------------------------------
+ */
+
+let administrators = ['Paul', 'Pierre', 'Mathilde']
+
+// Avant chaque ajout d'administrateur dans le tableau, tester si le nombre d'administrateurs est supérieur ou égal à 4, si c'est le cas, on enlève le dernier élément pour ajouter ensuite le nouvel administrateur. Si ce n'est pas le cas, ajouter simplement le nouvel administrateur.
+
+// let newAdmin1 = 'Marianne'
+// let newAdmin3 = 'Lucie'
+// let newAdmin2 = 'Sophie'
+
+// if (administrators.length >= 4) {
+//     administrators.pop()
+// }
+// administrators.push(newAdmin1)
+
+// if (administrators.length >= 4) {
+//     administrators.pop()
+// }
+// administrators.push(newAdmin2)
+
+// if (administrators.length >= 4) {
+//     administrators.pop()
+// }
+// administrators.push(newAdmin3)
+
+// console.log(administrators)
+
+
+
+// Ajouter utilisateur si la propriété admin est true
+let newAdmin1 = {
+    name: 'Marianne',
+    admin: true
+}
+
+let newAdmin2 = {
+    name: 'Sophie',
+    admin: false
+}
+
+let newAdmin3 = {
+    name: 'Lucie',
+    admin: true
+}
+
+if () {
+    administrators.push(newAdmin1)
+}
+
+if () {
+    administrators.push(newAdmin2)
+}
+
+if () {
+    administrators.push(newAdmin3)
+}
+
+console.log(administrators)
