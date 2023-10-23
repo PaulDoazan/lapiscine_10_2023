989e7d935ab028c64aa9cc179063a7134237e431
pdoazan <paul.doazan@gmail.com>
2023-10-23 17:31:27 +0200

    WIP bmi web-app

--- exercices/imc/index.js
+++ exercices/imc/index.js
@@ -1,1 +1,18 @@
-console.log('Hello world from index.js')
+// 1 On récupère la balise html et on la stocke dans une variable en js
+const validationBtn = document.querySelector(".validation-btn")
+const resultIMC = document.querySelector('.bmi-value')
+const inputs = document.querySelectorAll('.bmi-input')
+
+// 2 on déclare un click sur le bouton, à chaque fois qu'on clique, on exécute la fonction associéee
+validationBtn.addEventListener('click', onBtnClick)
+
+function onBtnClick() {
+    const height = inputs[0].value / 100
+    const weight = inputs[1].value
+    // afficher dans la console l'IMC => poids en kg / taille en m ² ** 2
+
+    // vérifier les données utilisateur, pas de valeurs <= 0
+    const bmi = weight / height ** 2
+
+    resultIMC.textContent = bmi
+}
