054791e725002e8710162986e8329435b753e51f
pdoazan <paul.doazan@gmail.com>
2023-10-24 17:31:46 +0200

    Correct exo 4 in dom-interaction

--- exercices/dom-interactions/starter/src/main.js
+++ exercices/dom-interactions/starter/src/main.js
@@ -6,3 +6,22 @@
 /**
  * EXO 2
  */
+
+/**
+ * EXO 3
+ */
+
+/**
+ * EXO 4
+ */
+
+const form2 = document.querySelector('.form-2')
+const input2 = document.querySelector('.input-2')
+const textToDisplay2 = document.querySelector('.text-to-display-2')
+
+form2.addEventListener('submit', onForm2Submit)
+
+function onForm2Submit(event) {
+    event.preventDefault();
+    textToDisplay2.textContent = input2.value
+}
