9846e358121d309ea2e52030b04fa3eb2af516e7
pdoazan <paul.doazan@gmail.com>
2023-10-24 12:25:22 +0200

    Add handleError function in BMI exercice

--- exercices/imc/index.js
+++ exercices/imc/index.js
@@ -1,6 +1,7 @@
 // 1 On récupère la balise html et on la stocke dans une variable en js
 const validationBtn = document.querySelector(".validation-btn")
 const resultIMC = document.querySelector('.bmi-value')
+const comment = document.querySelector('.comment')
 const inputs = document.querySelectorAll('.bmi-input')
 
 // 2 on déclare un click sur le bouton, à chaque fois qu'on clique, on exécute la fonction associéee
@@ -12,7 +13,20 @@ function onBtnClick() {
     // afficher dans la console l'IMC => poids en kg / taille en m ² ** 2
 
     // vérifier les données utilisateur, pas de valeurs <= 0
+    if (!weight || weight < 0) {
+        handleError('heightError')
+        return
+    }
+    if (!height || height < 0) {
+        handleError('weightError')
+        return
+    }
     const bmi = weight / height ** 2
 
     resultIMC.textContent = bmi
+}
+
+function handleError(typeError) {
+    // Implémenter la condition pour préciser le message d'erreur en fonction de la taille ou du poids
+    comment.textContent = "La valeur saisie dans le champ taille est incorrecte"
 }
