ab42eab556883438d4e4d9e788c0e247d04915fe
pdoazan <paul.doazan@gmail.com>
2023-10-24 15:34:28 +0200

    Achieve BMI exercice

--- exercices/imc/index.js
+++ exercices/imc/index.js
@@ -3,7 +3,8 @@ const BMIData = [
     { name: "Bonne santé", color: "green", range: [18.5, 25] },
     { name: "Surpoids", color: "lightcoral", range: [25, 30] },
     { name: "Obésité modérée", color: "orange", range: [30, 35] },
-    { name: "Obésité sévère", color: "crimson", range: [35, 40] }
+    { name: "Obésité sévère", color: "crimson", range: [35, 40] },
+    // { name: "Obésité morbide", color: "purple", range: [40, Number.POSITIVE_INFINITY] },
 ];
 
 // 1 On récupère la balise html et on la stocke dans une variable en js
@@ -16,6 +17,10 @@ const inputs = document.querySelectorAll('.bmi-input')
 validationBtn.addEventListener('click', onBtnClick)
 
 function onBtnClick() {
+    resultIMC.textContent = 0
+    resultIMC.style.color = 'black'
+    // resultIMC.style.fontSize = '30px'
+
     const height = inputs[0].value / 100
     const weight = inputs[1].value
     // afficher dans la console l'IMC => poids en kg / taille en m ² ** 2
@@ -27,7 +32,30 @@ function onBtnClick() {
     const bmi = weight / (height * height)
 
     resultIMC.textContent = bmi.toFixed(2)
-    comment.textContent = "..."
+
+    // Adapter l'affichage de l'interface, couleur de bmi-value et textContent de comment en fonction de la valeur de bmi
+    // 1. Sélectionner le bon élément dans le tableau
+    // 2. Appliquer le .color et textContent
+
+    let noLineFounded = true
+
+    for (let i = 0; i < BMIData.length; i++) {
+        if (bmi > BMIData[i].range[0] && bmi <= BMIData[i].range[1]) {
+            resultIMC.style.color = BMIData[i].color
+            // resultIMC.style.fontSize = bmi + 'px'
+            comment.textContent = BMIData[i].name
+            noLineFounded = false
+        }
+    }
+
+    if (noLineFounded) {
+        resultIMC.style.color = 'purple'
+        // resultIMC.style.fontSize = bmi + 'px'
+        comment.textContent = 'Obésité morbide'
+    }
+
+    // si l'imc est trop élevé,
+    // "Obésité morbide" => color: "purple"
 }
 
 function checkError(hParameter, wParameter) {
