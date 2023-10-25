f1ed9353f6fc51060c64f2c8cc17c1765c41b8de
pdoazan <paul.doazan@gmail.com>
2023-10-25 12:49:45 +0200

    Coorect exo 1 algo

--- exercices/algo_1/index.js
+++ exercices/algo_1/index.js
@@ -1,7 +1,25 @@
-const numbers = [12, 23, 5, 89, 8, 3, 104]
+const resultDiv = document.querySelector('.result')
+
+const numbers = [2, 12, 23, 5, 89, 8, 3, 104]
 
 // 1. Ecrire une fonction qui retourne la valeur minimum contenu dans un tableau passé en paramètre
 
-function getMinimum() {
+const result1 = getMinimum(numbers)
+resultDiv.textContent = result1
+
+function getMinimum(arr) {
+    let result = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < result) {
+            result = arr[i]
+        }
+    }
+
+    return result
+}
+
+// 2. Ecrire une fonction qui retourne la valeur maximum contenu dans un tableau passé en paramètre
+
+function getMaximum() {
 
 }
