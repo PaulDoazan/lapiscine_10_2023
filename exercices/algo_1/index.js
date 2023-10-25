157b2bdbd8b15e7af4c0ed8ab07abc2c3530a408
pdoazan <paul.doazan@gmail.com>
2023-10-25 14:45:06 +0200

    Add 3rd exercice algo

--- exercices/algo_1/index.js
+++ exercices/algo_1/index.js
@@ -23,6 +23,23 @@ function getMinimum(arr) {
 
 // 2. Ecrire une fonction qui retourne la valeur maximum contenu dans un tableau passé en paramètre
 
-function getMaximum() {
+const numbers4 = [3, 12, 135, 18, 136, 20]
 
+const res2 = getMaximum(numbers4)
+console.log(res2)
+
+function getMaximum(arrParam) {
+    let comparator = arrParam[0]
+    for (let n = 1; n < arrParam.length; n++) {
+        if (arrParam[n] > comparator) {
+            comparator = arrParam[n]
+        }
+    }
+
+    return comparator;
 }
+
+
+// 3. Ecrire une fonction qui prend en paramètre un tableau de nombre et qui retourne un nouveau tableau de nombres qui seront tous supérieurs à la moyenne des nombres du tableau en paramètres. 
+
+const nbs = [3, 5, 10]
