2219e19aab8a866e447129e48d075ed0b40fd5b1
pdoazan <paul.doazan@gmail.com>
2023-10-25 14:19:10 +0200

    Add numbers array

--- exercices/algo_1/index.js
+++ exercices/algo_1/index.js
@@ -1,21 +1,24 @@
 const resultDiv = document.querySelector('.result')
 
 const numbers = [2, 12, 23, 5, 89, 8, 3, 104]
+const numbers2 = [2, 11, 23, 5, 79, 8, 3, 105]
+const numbers3 = [2, 12, 23, 5, 89, 8, 1, 104]
 
 // 1. Ecrire une fonction qui retourne la valeur minimum contenu dans un tableau passé en paramètre
 
-const result1 = getMinimum(numbers)
-resultDiv.textContent = result1
+const min = getMinimum(numbers)
+resultDiv.textContent = min
 
 function getMinimum(arr) {
-    let result = arr[0];
+    let res = arr[0];
+
     for (let i = 1; i < arr.length; i++) {
-        if (arr[i] < result) {
-            result = arr[i]
+        if (arr[i] < res) {
+            res = arr[i]
         }
     }
 
-    return result
+    return res
 }
 
 // 2. Ecrire une fonction qui retourne la valeur maximum contenu dans un tableau passé en paramètre
