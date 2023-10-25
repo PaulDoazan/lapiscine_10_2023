044af84f269b17b9c5bc2740c316fb104df6d77d
pdoazan <paul.doazan@gmail.com>
2023-10-25 16:32:14 +0200

    Add pizzas data

--- exercices/algo_1/index.js
+++ exercices/algo_1/index.js
@@ -26,7 +26,7 @@ function getMinimum(arr) {
 const numbers4 = [3, 12, 135, 18, 136, 20]
 
 const res2 = getMaximum(numbers4)
-console.log(res2)
+// console.log(res2)
 
 function getMaximum(arrParam) {
     let comparator = arrParam[0]
@@ -42,4 +42,101 @@ function getMaximum(arrParam) {
 
 // 3. Ecrire une fonction qui prend en paramètre un tableau de nombre et qui retourne un nouveau tableau de nombres qui seront tous supérieurs à la moyenne des nombres du tableau en paramètres. 
 
-const nbs = [3, 5, 10] // => [10]
+const nbs = [7, 5, 3, 10] // => [7, 10]
+
+const nbsAbove = getNumbersAboveAverage(nbs)
+console.log(nbsAbove);
+
+function getNumbersAboveAverage(tab) {
+    // 1. On calcule le total
+    let total = 0
+
+    for (let index = 0; index < tab.length; index++) {
+        total += tab[index]
+    }
+
+    // 2. On calcule la moyenne (total / length)
+    let avg = total / tab.length
+
+    // 3. On filtre le tableau grace à la moyenne
+    let filteredNumbers = []
+
+    for (let index = 0; index < tab.length; index++) {
+        if (tab[index] > avg) {
+            filteredNumbers.push(tab[index])
+        }
+    }
+
+    return filteredNumbers;
+}
+
+
+// Exercice des pizzas
+const pizzas = [
+    {
+        name: 'Margherita',
+        price: 11.50,
+        ingredients: ['mozzarella', 'tomate', 'basilic ', "huile d'olive"],
+        baseTomate: true
+    },
+    {
+        name: 'Regina',
+        price: 12,
+        ingredients: ['mozzarella', 'tomate', 'origan', 'jambon', 'champignons'],
+        baseTomate: true
+    },
+    {
+        name: '4 saisons',
+        price: 15,
+        ingredients: ['artichaut', 'courgette', 'poivron', 'mozzarella', 'oignon rouge'],
+        baseTomate: true
+    },
+    {
+        name: 'Napolitaine',
+        price: 14,
+        ingredients: ['tomate', 'mozzarella', 'anchois', 'olives noires', 'origan'],
+        baseTomate: true
+    },
+    {
+        name: '4 fromages',
+        price: 16,
+        ingredients: ['tomate', 'mozzarella', 'emmental', 'comté', 'roquefort'],
+        baseTomate: true
+    },
+    {
+        name: 'Montagnarde',
+        price: 19,
+        ingredients: ['mozzarella', 'reblochon', 'gruyère', 'oignon', 'champignon'],
+        baseTomate: false
+    },
+    {
+        name: 'Chèvre-miel',
+        price: 18,
+        ingredients: ['mozzarella', 'chèvre', 'miel'],
+        baseTomate: false
+    },
+    {
+        name: 'Hawaïenne',
+        price: 17,
+        ingredients: ['mozzarella', 'tomate', 'jambon', 'ananas'],
+        baseTomate: true
+    }
+]
+
+const pizzas2 = [
+    {
+        name: '8 fromages',
+        price: 10,
+        ingredients: ['mozzarella', 'reblochon', 'gruyère', 'oignon', 'champignon'],
+        baseTomate: false
+    },
+    {
+        name: '12 fromages',
+        price: 18,
+        ingredients: ['mozzarella', 'chèvre', 'miel'],
+        baseTomate: false
+    },
+]
+
+// Ecrire une fonction qui prend en paramètre un tableau de pizzas, et qui retourne un tableau de tous les noms des pizzas
+
