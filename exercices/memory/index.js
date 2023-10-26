03874cc6e8a286919af196e036abc846ed16523a
pdoazan <paul.doazan@gmail.com>
2023-10-26 16:33:01 +0200

    Update memory

--- exercices/memory/index.js
+++ exercices/memory/index.js
@@ -2,6 +2,9 @@ const title = document.querySelector('.title')
 const cards = document.querySelectorAll('.card')
 
 for (let i = 0; i < cards.length; i++) {
+    console.log(cards[i].children)
+    cards[i].children[0].children[0].textContent = Math.round((i + 1) / 2)
+    cards[i].children[1].children[0].textContent = "?"
     cards[i].addEventListener('click', onCardClick)
 }
 
@@ -9,5 +12,10 @@ function onCardClick(event) {
     /**
      * evet.target est l'élément html sur lequel l'utilisateur a cliqué, event.currentTarget est l'élément html sur lequel .addEventListener a été déclaré
      */
-    console.log(event.currentTarget)
+    const tg = event.currentTarget
+    if (tg.classList.contains('is-flipped')) {
+        tg.classList.remove('is-flipped')
+    } else {
+        tg.classList.add('is-flipped')
+    }
 }
