7932a2872872577ab47b425d1bc9faf06f0dd8c4
pdoazan <paul.doazan@gmail.com>
2023-10-26 14:48:47 +0200

    Update css and html template for memory

--- exercices/memory/index.js
+++ exercices/memory/index.js
@@ -1,1 +1,6 @@
 const title = document.querySelector('.title')
+
+var card = document.querySelector('.card');
+card.addEventListener('click', function () {
+    card.classList.toggle('is-flipped');
+});
