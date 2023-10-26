24c4889847b018b1b6d319b4fe060a1920cca05f
pdoazan <paul.doazan@gmail.com>
2023-10-26 15:39:20 +0200

    loop on cards to declare addEventListener

--- exercices/memory/index.js
+++ exercices/memory/index.js
@@ -1,6 +1,13 @@
 const title = document.querySelector('.title')
+const cards = document.querySelectorAll('.card')
 
-var card = document.querySelector('.card');
-card.addEventListener('click', function () {
-    card.classList.toggle('is-flipped');
-});
+for (let i = 0; i < cards.length; i++) {
+    cards[i].addEventListener('click', onCardClick)
+}
+
+function onCardClick(event) {
+    /**
+     * evet.target est l'élément html sur lequel l'utilisateur a cliqué, event.currentTarget est l'élément html sur lequel .addEventListener a été déclaré
+     */
+    console.log(event.currentTarget)
+}
