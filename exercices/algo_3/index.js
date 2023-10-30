c76cafeda78f442c22b219ae4bc368196a3dae93
pdoazan <paul.doazan@gmail.com>
2023-10-30 15:02:49 +0100

    map method

--- exercices/algo_3/index.js
+++ exercices/algo_3/index.js
@@ -20,7 +20,7 @@ movies.forEach((element) => {
     }
 })
 
-console.log(bestMovie.title)
+// console.log(bestMovie.title)
 
 // e. Logger le titre du film le plus ancien
 let oldestMovie = movies[0]
@@ -31,10 +31,11 @@ movies.forEach((element) => {
     }
 })
 
-console.log(oldestMovie.title);
+// console.log(oldestMovie.title);
 
 // exercices fondamentaux
 let arr = [1, 12, 8, 2, 5]
+
 // calcul de la somme du premier et denrier élément du tableau
 
 // f. Logger un tableau de tous les titres de film qui ont au moins 3 acteurs
@@ -46,4 +47,55 @@ movies.forEach((element) => {
     }
 })
 
-console.log(movieTitles)
+// console.log(movieTitles)
+
+// 1. Ecrire une fonction qui prend en parametre le tableau movies et qui retourne un nouveau tableau de tous les titres de films
+
+function getAllTitles(arr) {
+    let titles = []
+
+    arr.forEach((element) => {
+        titles.push(element.title)
+    })
+
+    return titles
+}
+
+const res = getAllTitles(movies)
+// console.log(res)
+
+
+
+
+// fondamentaux
+let arr2 = ['Salut', 'tout', 'le', 'monde']
+function createSentence() {
+
+}
+
+const dirs = getAllDirectors(movies)
+console.log(dirs)
+
+function getAllDirectors(films) {
+    // let directors = []
+
+    // arr.forEach((element) => {
+    //     directors.push(element.director)
+    // })
+
+    // return directors
+
+
+    // ------------------------------------
+    // let directors = films.map((element) => {
+    //     return element.title
+    // })
+
+    // return directors
+
+    return films.map((element) => element.director)
+}
+
+function getAllYears(arr) {
+
+}
