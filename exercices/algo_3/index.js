6b9660263afbca65947b3e76a34bc0e2f936482b
pdoazan <paul.doazan@gmail.com>
2023-10-30 11:42:03 +0100

    Add instructions

--- exercices/algo_3/index.js
+++ exercices/algo_3/index.js
@@ -1,3 +1,11 @@
 import movies from './movies.json' assert {type: 'json'}
 
-console.log(movies[0].title)
+// a. Logger le premier element du tableau movies
+
+// b. Logger l'annee du 4eme element du tableau movies
+
+// c. Logger le titre du dernier element du tableau movies
+
+// d. Logger le titre du film qui a la meilleure note
+
+// e. Logger le titre du film le plus ancien
