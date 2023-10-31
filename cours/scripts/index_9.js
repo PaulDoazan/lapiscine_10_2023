d77dc0e9f66530ec533604707e8b3b4be41a1bdf
pdoazan <paul.doazan@gmail.com>
2023-10-31 10:52:03 +0100

    Update index_9.js

--- cours/scripts/index_9.js
+++ cours/scripts/index_9.js
@@ -10,6 +10,9 @@ let arrClothesAtPlural = arr.map(el => el + 's')
 
 // arr.forEach((el) => arrClothesAtPlural.push(el + 's'))
 
+/**
+ * Précisions sur la méthode .map() des tableaux
+ */
 let clothes = arr.map(el => {
     if (el.charAt(0) === "C") {
         return el
@@ -29,4 +32,30 @@ console.log(word.length)
  * méthode .filter()
  */
 
-// A l'aide de la méthode .filter, récupérer un nouveau tableau qui contient les mots de plus de 7 caractères
+// Exo : A l'aide de la méthode .filter, récupérer un nouveau tableau qui contient les mots de plus de 7 caractères
+
+const result = arr.filter(element => element.length > 7)
+console.log(result);
+
+if (null === undefined) {
+    console.log("Salut")
+} else {
+    console.log('Au revoir')
+}
+
+/**
+* Précisions sur la méthode .getAttribute des éléments html
+ */
+const paragraphs = document.querySelectorAll('.my-paragraph')
+
+paragraphs.forEach((paragraph) => {
+    paragraph.addEventListener('mouseover', (e) => {
+        e.currentTarget.style.backgroundColor = e.currentTarget.getAttribute('data-hello')
+    })
+})
+
+/**
+ * Précision sur les fonctions fléchées
+ */
+const title = document.querySelector('.my-title')
+title.addEventListener('click', () => console.log('click sur titre'))
