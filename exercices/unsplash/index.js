f9ec6cde23291743b404b83601a570d50e2e72f8
pdoazan <paul.doazan@gmail.com>
2023-11-02 16:27:18 +0100

    Update memory unsplash

--- exercices/unsplash/index.js
+++ exercices/unsplash/index.js
@@ -37,23 +37,34 @@ function displayData(data) {
     // On créé un élément html en javascript
     for (let i = 0; i < 8; i++) {
         console.log(data.results[i])
+        // const newDiv = document.createElement("div")
+        // newDiv.style.position = "relative"
+        // const newImg = document.createElement("img")
+        // const title = document.createElement("h3")
+        // // newImg.style.position = "absolute"
+        // title.style.position = "absolute"
+        // title.style.color = "white"
+        // title.style.margin = '0.5rem'
+        // title.style.opacity = 0.6
+
+        // title.textContent = data.results[i].alt_description
+        // // title.textContent = data.results[i]
+        // newImg.src = data.results[i].urls.regular
+
+        // newDiv.appendChild(title)
+        // newDiv.appendChild(newImg)
+
         const newDiv = document.createElement("div")
-        newDiv.style.position = "relative"
-        const newImg = document.createElement("img")
-        const title = document.createElement("h3")
-        // newImg.style.position = "absolute"
-        title.style.position = "absolute"
-        title.style.color = "white"
-        title.style.margin = '0.5rem'
-        title.style.opacity = 0.6
-
-        title.textContent = data.results[i].alt_description
-        // title.textContent = data.results[i]
-        newImg.src = data.results[i].urls.regular
-
-        newDiv.appendChild(title)
-        newDiv.appendChild(newImg)
-
+        // is-flipped 
+        newDiv.className = "card"
+        newDiv.innerHTML = `
+            <div class="card__face card__face--front">
+                <img src="${data.results[i].urls.regular}" alt="">
+            </div>
+            <div class="card__face card__face--back">
+                <div class="card-text">?</div>  
+            </div>
+        `
         imgContainer.appendChild(newDiv)
 
         // const newImg2 = document.createElement("img")
